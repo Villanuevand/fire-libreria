78f45a083dc3695292ddd5ae79dc485fcbcab581
Jorge Cano <jorgeucano@gmail.com>
2018-09-27 16:15:44 -0300

    add table with mockup data for the libreria

--- src/app/libreria/libreria.module.ts
+++ src/app/libreria/libreria.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatTableModule } from '@angular/material';
 import { LibreriaRoutingModule } from './libreria-routing.module';
 import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    LibreriaRoutingModule
+    LibreriaRoutingModule,
+      MatTableModule
   ],
   declarations: [ListadoComponent]
 })
