e719b360f72f9c1d63680baf01c92322f12b118d
Jorge Cano <jorgeucano@gmail.com>
2018-09-27 15:34:23 -0300

    add router!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import {environment} from '../environments/environment';
+import {AppRoutingModule} from './router.module';
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +11,8 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import {environment} from '../environments/environment';
+import {RouterModule} from '@angular/router';
+
 
 const DECLARATIONS = [
     AppComponent
@@ -18,6 +21,8 @@ const DECLARATIONS = [
 const IMPORTS = [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
+    RouterModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFireModule.initializeApp(environment.firebase, 'fire-libreria'),
