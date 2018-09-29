46c568dac82d5c3d05ae3888f42688b175cdd366
Jorge Cano <jorgeucano@gmail.com>
2018-09-29 00:07:44 -0300

    add image support

--- src/app/abm/abm.component.ts
+++ src/app/abm/abm.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-abm',
@@ -10,6 +12,8 @@ import {map} from 'rxjs/operators';
 })
 export class AbmComponent implements OnInit {
 
+    uploadPercent: Observable<number>;
+    downloadURL: Observable<string>;
     books: Observable<any[]>;
     bookSelected: any = {};
     numero: string;
@@ -18,7 +22,7 @@ export class AbmComponent implements OnInit {
     cantidad: string;
     precio: string;
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private storage: AngularFireStorage) {
       // obtengo la lista de libros
       this.books = this.db.collection('book').snapshotChanges().pipe(
           map((collection) => {
@@ -37,24 +41,39 @@ export class AbmComponent implements OnInit {
   ngOnInit() { }
 
   save() {
-      const data = {
-          numero: this.numero,
-          titulo: this.titulo,
-          fecha: this.fecha,
-          cantidad: this.cantidad,
-          precio: this.precio
-      };
-    this.db.collection('book').add(data).then(
-        (response) => {
-            console.log(response);
-            document.getElementById('closeAgregar').click();
-        },
-        (error) => {
-            console.log(error);
-            document.getElementById('closeAgregar').click();
-        }
-    );
+      let imgUrl = 'https://www.freeiconspng.com/uploads/no-image-icon-6.png';
+      if (this.downloadURL) {
+          this.downloadURL.subscribe(
+              (response) => {
+                  imgUrl = response;
+                  this.saveAll(imgUrl);
+              }
+          );
+      } else {
+          this.saveAll(imgUrl);
+      }
   }
+
+    saveAll(imgUrl) {
+        const data = {
+            numero: this.numero,
+            titulo: this.titulo,
+            fecha: this.fecha,
+            cantidad: this.cantidad,
+            precio: this.precio,
+            urlImg: imgUrl
+        };
+        this.db.collection('book').add(data).then(
+            (response) => {
+                console.log(response);
+                document.getElementById('closeAgregar').click();
+            },
+            (error) => {
+                console.log(error);
+                document.getElementById('closeAgregar').click();
+            }
+        );
+    }
 
   delete() {
     this.db.doc(`book/${this.bookSelected.id}`).delete().then(
@@ -82,4 +101,18 @@ export class AbmComponent implements OnInit {
     );
   }
 
+    uploadFile(event) {
+        const file = event.target.files[0];
+        const filePath = 'img';
+        console.log(file);
+        console.log(filePath);
+        const fileRef = this.storage.ref(filePath);
+        const task = this.storage.upload(filePath, file);
+        // observe percentage changes
+        this.uploadPercent = task.percentageChanges();
+        // get notified when the download URL is available
+        task.snapshotChanges().pipe(
+            finalize(() => this.downloadURL = fileRef.getDownloadURL() )
+        ).subscribe();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 // fireImports
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-// import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { AbmComponent } from './abm/abm.component';
@@ -17,7 +17,8 @@ import { ListadoComponent } from './listado/listado.component';
 const FIREBASE_IMPORTS = [
   AngularFireModule.initializeApp(environment.firebase, 'fire-libreria'),
   AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFireStorageModule
 ];
 
 @NgModule({
